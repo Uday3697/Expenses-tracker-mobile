fe2329908bc5777220e4cca876ea8d1c7a2cc3fd
uday <web.uday@spaarksweb.com>
2024-05-16 17:32:28 +0530

    changes in lists dummy data added for test

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,16 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import ExpensesSummary from './ExpensesSummary'
+import ExpensesList from './ExpensesList'
 
-const ExpensesOutput = () => {
-  return (
-    <View>
+
+const Dummy_EXPENSES = [
+    {
+        id: 'e1',
+        description:'a pair of shoes',
+        amount :3424.4534,
+        date:new Date('2024-04-13')
+    },
+    {
+        id: 'e2',
+        description:'a pair of shoes',
+        amount :3424.4534,
+        date:new Date('2024-04-12')
+    },
+    {
+        id: 'e3',
+        description:'a pair sleeprs',
+        amount :342.4534,
+        date:new Date('2024-04-11')
+    },
+    {
+        id: 'e4',
+        description:'book',
+        amount :544.4534,
+        date:new Date('2024-04-1')
+    },
+]
+
+
+
+
+
+const ExpensesOutput = ({ expenses, expensesPeriod }) => {
+    return (
         <View>
-            <Text>Period</Text>
-            <Text>rsss</Text>
+            <ExpensesSummary expenses={Dummy_EXPENSES} expensesPeriod={expensesPeriod} />
+            <ExpensesList />
         </View>
-      <Text>ExpensesOutput</Text>
-    </View>
-  )
+    )
 }
 
 export default ExpensesOutput
